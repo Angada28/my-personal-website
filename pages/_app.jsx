08cb42e502013c71183999d07f3a733d35d218ea
Angada28 <angad2011@hotmail.com>
2022-02-28 15:09:50 -0500

    name shows up on tab

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,7 +1,15 @@
 import NavBar from '../components/Navbar';
 import '../styles/globals.css';
 
+import React, { useEffect } from 'react'
+import ReactDOM from 'react-dom'
+
 function MyApp({ Component, pageProps }) {
+
+  useEffect(() => {
+    document.title = "Angad Harish"
+  }, [])
+  
   return (
     <div>
       <NavBar />
