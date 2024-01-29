ac1f7f4b04786a313feeaca477a4a505350f94b4
Angada28 <angad2011@hotmail.com>
2024-01-29 09:14:27 -0500

    Revert "name shows up on tab"
    This reverts commit 08cb42e502013c71183999d07f3a733d35d218ea.

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,15 +1,7 @@
 /*import NavBar from '../components/Navbar';
 import '../styles/globals.css';
 
-import React, { useEffect } from 'react'
-import ReactDOM from 'react-dom'
-
 function MyApp({ Component, pageProps }) {
-
-  useEffect(() => {
-    document.title = "Angad Harish"
-  }, [])
-  
   return (
     <div>
       /<NavBar />
