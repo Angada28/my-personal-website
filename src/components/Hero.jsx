238f11b80b0d3f82ef07f649b2d3b9b343ce5fd5
Angad H <angad2011@hotmail.com>
2025-01-07 10:45:29 -0500

    updating hero styling to display image to the left of text and adding placeholder image

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,14 +2,16 @@ const Hero = () => {
   return (
     <>
       <div className="hero">
-        <p>picture of me on the left or right of this</p>
-        <h3>Angad Harish</h3>
-        <p>effect that changes text every few seconds</p>
-        <p>University of Toronto</p>
-        <p>Bachelors of Computer Science</p>
-        <p>A software engineer with experience in backend engineering and full-stack web development. </p>
-        <button>Random Fact about myself</button>
-        {/*TODO: give random fact button its own styling */}
+        <img src="/placerholder.jpg" alt="Angad Harish" className="hero-image" />
+        <div className="hero-text">
+          <h3>Angad Harish</h3>
+          <p>effect that changes text every few seconds</p>
+          <p>University of Toronto</p>
+          <p>Bachelors of Computer Science</p>
+          <p>A software engineer with experience in backend engineering and full-stack web development. </p>
+          <button>Random Fact about myself</button>
+          {/*TODO: give random fact button its own styling */}
+        </div>
       </div>
     </>
   );
